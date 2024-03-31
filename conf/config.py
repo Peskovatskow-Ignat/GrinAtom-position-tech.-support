2fa4946b5c2eb6ab67e59b652da874421a36bfd1
Ignat <pesk2305@yandex.ru>
2024-03-31 17:55:37 +0300

    chenge import

--- conf/config.py
+++ conf/config.py
@@ -7,14 +7,14 @@ load_dotenv()
 
 class Settings:
 
-    CURRENCY_PAIR_1: str = 'GBP/RUB'
-    CURRENCY_PAIR_2: str = 'JPY/RUB'
-    
-    NUMBER_FORMAT: str = '##0.00 ₽ '
+    CURRENCY_PAIR_1: str = "GBP/RUB"
+    CURRENCY_PAIR_2: str = "JPY/RUB"
 
-    SMTP_USERNAME: str = getenv('SMTP_USERNAME')
-    EMAIL_PASSWORD: str = getenv('EMAIL_PASSWORD')
-    RECIPIENT_EMAIL: str = getenv('RECIPIENT_EMAIL')
+    NUMBER_FORMAT: str = "##0.00 ₽ "
+
+    SMTP_USERNAME: str = getenv("SMTP_USERNAME")
+    EMAIL_PASSWORD: str = getenv("EMAIL_PASSWORD")
+    RECIPIENT_EMAIL: str = getenv("RECIPIENT_EMAIL")
 
 
 settings = Settings()
--- utils/update_tables.py
+++ utils/update_tables.py
@@ -3,8 +3,8 @@ from typing import Any
 import pandas as pd
 from openpyxl.styles import NamedStyle
 
-from utils.logger import logger
 from conf.config import settings
+from utils.logger import logger
 
 
 @logger
@@ -13,9 +13,10 @@ def cell_alignment(worksheet: list[dict[str, Any]], df_cours: pd.DataFrame) -> N
         column_len = max(df_cours[col].astype(str).map(len).max(), len(col)) + 1
         worksheet.column_dimensions[chr(65 + i)].width = column_len
 
+
 @logger
 def apply_style(worksheet: dict[str, Any], df_cours: pd.DataFrame) -> None:
-    money_style = NamedStyle(name='money', number_format=settings.NUMBER_FORMAT)
-    for col in ['C', 'F', 'G']:
+    money_style = NamedStyle(name="money", number_format=settings.NUMBER_FORMAT)
+    for col in ["C", "F", "G"]:
         for row in range(2, len(df_cours) + 2):
             worksheet[f"{col}{row}"].style = money_style
